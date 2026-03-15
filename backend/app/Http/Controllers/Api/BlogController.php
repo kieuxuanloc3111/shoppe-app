e40f2143340f888380bf8c2927b38312e96a7029
kieuxuanloc3111 <chokeslam311@gmail.com>
2026-03-15 20:43:31 +0700

    comment api

---
+++ backend/app/Http/Controllers/Api/BlogController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class BlogController extends Controller
+{
+    //
+    public $successStatus = 200;
+
+    public function index()
+    {
+        $blogs = Blog::orderBy('created_at','desc')->paginate(5);
+
+        return response()->json([
+            'blog' => $blogs
+        ], $this->successStatus);
+    }
+    public function detail($id){
+        $blog = Blog::find($id);
+        if (!$blog) {
+        return response()->json([
+            'response' => 'error',
+            'message' => 'Blog not found'
+        ], 404);
+        }
+
+        return response()->json([
+            'response' => 'success',
+            'data' => $blog
+        ], $this->successStatus);
+    }
+    public function getComment($id)
+    {
+        $comments = Comment::where('blog_id',$id)
+                    ->orderBy('created_at','asc')
+                    ->get();
+
+        return response()->json([
+            'response' => 'success',
+            'data' => $comments
+        ],200);
+    }
+    public function storeComment(Request $request, $id)
+    {
+        $user = Auth::user();
+
+        if(!$user){
+            return response()->json([
+                'response' => 'error',
+                'message' => 'Unauthenticated'
+            ],401);
+        }
+
+        // xác định level comment
+        $level = 0;
+        if($request->id_comment != 0){
+            $level = 1;
+        }
+
+        $comment = Comment::create([
+            'blog_id' => $id,
+            'user_id' => $request->id_user,
+            'parent_id' => $request->id_comment == 0 ? null : $request->id_comment,
+            'level' => $level,
+            'content' => $request->comment,
+            'user_name' => $request->name_user,
+            'user_avatar' => $request->image_user
+        ]);
+
+        return response()->json([
+            'response' => 'success',
+            'data' => $comment
+        ],200);
+    }
+    
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MemberController;
-
+use App\Http\Controllers\Api\BlogController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 
@@ -15,9 +15,17 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user/my-product', [ProductController::class, 'myProduct']);
 
     Route::get('/user/product/delete/{id}', [ProductController::class, 'deleteProduct']);
+    Route::post('/blog/comment/{id}', [BlogController::class,'storeComment']);
 });
 
 Route::get('/product', [ProductController::class, 'product']);
 Route::get('/product/detail/{id}', [ProductController::class, 'detail']);
 Route::post('/product/cart',[ProductController::class, 'productCart']);
+
+
+
+Route::get('/blog', [BlogController::class,'index']);
+Route::get('/blog/detail/{id}', [BlogController::class,'detail']);
+
+Route::get('/blog/comment/{id}', [BlogController::class,'getComment']);
 ?>
